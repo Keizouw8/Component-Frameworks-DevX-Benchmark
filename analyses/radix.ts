20503360849f649c1a2639966bcd2b5584cc508f
Keizou Wang <keizouw@proton.me>
2026-05-08 17:27:05 -0700

    optimize radix log

--- analyses/radix.ts
+++ analyses/radix.ts
@@ -1,4 +1,4 @@
 export default function radixEconomy({ tokens: { length: n }, unique: { length: b } }: Program): number {
 	if (b == 1) return n;
-	return b * Math.floor(Math.log(n) / Math.log(b) + 1);
+	return b * Math.floor(Math.log2(n) / Math.log2(b) + 1);
 }
