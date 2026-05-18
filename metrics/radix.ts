7586151724992aa1f5ed954efbbefc082c644625
Keizou Wang <keizouw@proton.me>
2026-05-18 09:08:25 -0700

    new radix formula

--- metrics/radix.ts
+++ metrics/radix.ts
@@ -1,4 +1,3 @@
 export default function radixEconomy({ tokens: { length: n }, unique: { length: b } }: Program): number {
-	if (b == 1) return n;
-	return b * Math.floor(Math.log2(n) / Math.log2(b) + 1);
+	return b * n;
 }
